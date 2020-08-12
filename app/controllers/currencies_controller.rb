f84d0550fc3c31e873e6163fd4b3a31e4109dfb6
hydropero <mylesjbulla@gmail.com>
2020-08-12 06:58:16 +0000

    finished 1st half.

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,8 +3,31 @@ class CurrenciesController < ApplicationController
     @raw_data = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch('symbols')
-    
+    @symbols_keys = @symbols_hash.keys
     render({ :template => 'currency_templates/step_one.html.erb'})
     
+    
+  end
+
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch('symbols')
+    @symbols_keys = @symbols_hash.keys
+    @selected_symbol = params.fetch('symbol')
+
+    render({ :template => 'currency_templates/step_two.html.erb'})
+  end
+
+  def third_currency
+    @first_symbol = params.fetch('symbol')
+    @second_symbol = params.fetch('symbol_2')
+    url = ("https://api.exchangerate.host/convert?from=" + @first_symbol + "&to=" + @second_symbol)
+    @raw_data = open(url).read
+    @parsed_data = JSON.parse(@raw_data)
+    @results = @parsed_data.fetch('info')
+    @final_result = @results.fetch('rate')
+    
+    render({ :template => 'currency_templates/step_three.html.erb'})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/", { :controller => 'application', :action => 'dashboard_options'})
-  get('/fer', { :controller => 'currencies', :action => 'first_currency'})
-
+  get('/forex', { :controller => 'currencies', :action => 'first_currency'})
+  get('/forex/:symbol', { :controller => 'currencies', :action => 'second_currency'})
+  get('forex/:symbol/:symbol_2', { :controller => 'currencies', :action => 'third_currency'})
 
 end
